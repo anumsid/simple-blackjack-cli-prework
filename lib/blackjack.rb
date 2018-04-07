11a2bc03ee1194d435b750ed52193b585d2746c0
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-04-07 01:28:57 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -52,5 +52,11 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  initial_round
+  until initial_round > 21
+    card_total = hit?(initial_round)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
 end
